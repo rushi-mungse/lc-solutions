f49b41f201c1f0ee96366f509a0a345742eef779
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-18 17:54:56 +0530

    add links

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -7,12 +7,12 @@ export function SiteFooter() {
         <p className="text-balance text-center text-sm leading-loose text-foreground md:text-left">
           Built by{" "}
           <a
-            href={siteConfig.links.twitter}
+            href={siteConfig.links.linkedin}
             target="_blank"
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            {siteConfig.name}
+            @rushi_mungse
           </a>
           . The source code is available on{" "}
           <a
